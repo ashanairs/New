71ccb977584732d3c011dbadb009cc00d58e407c
ashanairs <ashanair605@gmail.com>
2025-05-24 08:57:14 +0530

    Navbar.jsx

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div><Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+           
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            User Log
+          </Typography>
+          <Link to={'/'}><Button style={{color:'white'}}>Home</Button></Link>
+          <Link to={'/add'}><Button style={{color:'white'}}>Add</Button></Link>
+        </Toolbar>
+      </AppBar>
+    </Box></div>
   )
 }
 
