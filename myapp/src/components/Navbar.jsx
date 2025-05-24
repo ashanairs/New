e62d5f0ccd75e164e8f3570279731c6379c28175
ashanairs <ashanair605@gmail.com>
2025-05-24 09:01:48 +0530

    Navbar.jsx

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 
 const Navbar = () => {
   return (
