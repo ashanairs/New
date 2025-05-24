bcb002965a8392d02e29104f68b7d3554070ca98
ashanairs <ashanair605@gmail.com>
2025-05-24 18:29:38 +0530

    main.jsx

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -9,9 +9,11 @@ import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
-    <Navbar></Navbar>
+    <Navbar><Navbar/>
     <Home></Home>
     <Add></Add>
+    </BrowserRouter>
   </StrictMode>,
 )
